2fe8d3e9c875666f6ddf2858530e10dbb3d63414
danilmhk <aaa@aaa.aa>
2026-03-26 13:02:45 +0700

    добавлена модель исполнителя

--- Project/main/models.py
+++ Project/main/models.py
@@ -7,9 +7,17 @@ class Genres(models.Model):
     def __str__(self):
         return self.name_en
     
+
+class Artist(models.Model):
+    name = models.CharField(max_length=500, unique = True)
+    image = models.ImageField(upload_to='artists/', null=True)
+    def __str__(self):
+        return self.name 
+    
 class Track(models.Model):
     title = models.CharField(max_length = 200, unique = True)
     duration = models.CharField(max_length = 200)
     genre = models.ManyToManyField(Genres)
+    artist = models.ForeignKey(Artist, on_delete=models.RESTRICT, null=True)
     def __str__(self):
         return self.title
